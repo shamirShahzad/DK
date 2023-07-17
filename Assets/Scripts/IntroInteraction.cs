cdb332b0126359c91b8b42182856d454939c6c23
shamirShahzad <shameershahzad2@gmail.com>
2023-07-17 10:20:45 +0500

    Better Interaction for Intro Level

--- Assets/Scripts/IntroInteraction.cs
+++ Assets/Scripts/IntroInteraction.cs
@@ -1,8 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 namespace DK
 {
-    public class IntroInteraction : Interactable
+    public class IntroInteraction : MonoBehaviour
     {
-
+        [SerializeField] string actionText;
+        [SerializeField] GameObject intro;
+        [SerializeField] TextMeshProUGUI introText;
+        [SerializeField] Image buttonIcon;
+        [SerializeField] Sprite buttonIconSprite;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.name.Contains("Player"))
+            {
+                intro.SetActive(true);
+                introText.text = actionText;
+                buttonIcon.sprite = buttonIconSprite;
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.name.Contains("Player"))
+            {
+                intro.SetActive(false);
+                introText.text = "NULL";
+                buttonIcon.sprite = null;
+            }
+        }
     }
 }
